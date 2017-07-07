60b9c87ba77cca56318b2a3d0f9b75d8f625bfa1
Yang Nie <yang.nie@gmail.com>
2017-07-07 14:38:01 -0400

    style: format code

--- code/decision.py
+++ code/decision.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 # This is where you can build a decision tree for determining throttle, brake and steer 
 # commands based on the output of the perception_step() function
 def decision_step(Rover):
